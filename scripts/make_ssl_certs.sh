874638e399216b8670bf28330335817803d84e17
Nikhil John <nikhiljohn1010@gmail.com>
2022-09-02 20:27:38 +0530

    Update make_ssl_certs.sh

--- scripts/make_ssl_certs.sh
+++ scripts/make_ssl_certs.sh
@@ -8,5 +8,3 @@ openssl req -x509 -out $CERT_PATH/cert.pem -keyout $CERT_PATH/key.pem \
   -newkey rsa:2048 -nodes -sha256 \
   -subj '/CN=localhost' -extensions EXT -config <( \
    printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
-
-docker compose -f "../docker-compose.keyclock.yml" up -d
