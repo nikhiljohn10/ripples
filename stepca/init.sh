abd45f4040939ee6a1d0e47db93026a6d50008db
Nikhil John <nikhiljohn1010@gmail.com>
2022-09-20 16:11:56 +0530

    Added ACME provider in stepca

--- stepca/init.sh
+++ stepca/init.sh
@@ -2,9 +2,11 @@
 
 # Remove old containers and volumes if `-f` option given
 if test "$1" = "-f"; then
-    docker compose down 2>/dev/null && \
-    docker volume prune -f
+    docker compose down 2>/dev/null
+    docker volume rm stepca_data -f
 fi
 
 # Start docker stack
 docker compose up -d || exit 1
+sleep 4 && docker compose exec ca step ca provisioner add acme --type ACME
+docker compose restart
